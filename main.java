b79dfc1e5df52cafeee5587201ee52f4b9a808df
portal <gabriel.o.portal@gmail.com>
2018-06-14 15:09:07 -0300

    att

--- main.java
+++ main.java
@@ -13,8 +13,19 @@ public class main{
         frm.show();
     }
 
+    private static ImageIcon criarImagens(){
+        int width=900,height=600;
+        BufferedImage buffer = new BufferedImage( width, height, BufferedImage.TYPE_INT_RGB );
+        Graphics g = buffer.createGraphics();
+        g.setColor( Color.WHITE );
+        g.fillRect( 0, 0, width, height );
+        g.setColor(Color.BLACK);
+        g.drawImage(criarImagem(), 100, 1000, ImageObserver observer);
+    }
+
     private static ImageIcon criarImagem() {
-        int width=1024, height=768;
+        // int width=1024, height=768;
+        int width=300,height=300;
         BufferedImage buffer = new BufferedImage( width, height, BufferedImage.TYPE_INT_RGB );
         Graphics g = buffer.createGraphics();
         g.setColor( Color.WHITE );
@@ -23,8 +34,9 @@ public class main{
         // g.drawLine( 0, 0, width, height );
         g.drawOval(50, 50, 50, 50);
         g.drawRect(100, 100, 100, 100);
+        g.create(150, 100, 250, 300);
         // drawRect(int x, int y, int width, int height)
         return new ImageIcon( buffer );
     }
 
-} 
+}
