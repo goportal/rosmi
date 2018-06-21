fed4f1b31cf366aab26d169720e8ecbdf42ff8b6
portal <gabriel.o.portal@gmail.com>
2018-06-21 17:19:20 -0300

    arquivo main restante

--- main.java
+++ main.java
@@ -9,25 +9,28 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-public class main{
 
-    public static void main(String args[]){
+public class main{
+    static int aux = 0;
+    public static void main(String []args){
+
         JFrame f = new JFrame("ROSMI");
-        JLabel label1, label2, label3, label4,label6, label5;
+        JLabel label1, label2, label3, label4, label5, label6, label7, label8, label9;
         JPanel subPanel = new JPanel();
         // subPanel.setPreferredSize (new Dimension(400, 400));
         subPanel.setBackground (Color.BLACK);
 
         //ImageIcon imgicon = new ImageIcon(criarImagem()) ;
 
-        ImageIcon imgicon = criarImagem();
-
-        label1 = new JLabel (imgicon);
-        label2 = new JLabel (imgicon);
-        label3 = new JLabel (imgicon);
-        label4 = new JLabel (imgicon);
-        label5 = new JLabel (imgicon);
-        label6 = new JLabel (imgicon);
+        label1 = new JLabel (criarImagem());
+        label2 = new JLabel (criarImagem());
+        label3 = new JLabel (criarImagem());
+        label4 = new JLabel (criarImagem());
+        label5 = new JLabel (criarImagem());
+        label6 = new JLabel (criarImagem());
+        label7 = new JLabel (criarImagem());
+        label8 = new JLabel (criarImagem());
+        label9 = new JLabel (criarImagem());
         
         subPanel.add (label1);
         subPanel.add (label2);
@@ -35,42 +38,22 @@ public class main{
         subPanel.add (label4);
         subPanel.add (label5);
         subPanel.add (label6);
+        subPanel.add (label7);
+        subPanel.add (label8);
+        subPanel.add (label9);
         
         f.setSize(917,917);
         f.setVisible(true);  
         f.add(subPanel);
-        System.out.println("...Runing...");
-    }
 
-    // public static void main(String[]args){
-    //     JFrame frm = new JFrame("Rosmi");
-    //     JPanel pan = new JPanel();
-    //     JLabel lbl = new JLabel( criarImagem() );
-    //     pan.add( lbl );
-    //     pan.add( lbl );
-    //     frm.getContentPane().add( pan );
-    //     frm.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-    //     frm.pack();
-    //     frm.show();
-    // }
+        System.out.println("...Runing...");
+
+    }
 
     private static ImageIcon criarImagem() {
-        // int width=1024, height=768;
-        int width=300,height=300;
-        BufferedImage buffer = new BufferedImage( width, height, BufferedImage.TYPE_INT_RGB );
-        Graphics g = buffer.createGraphics();
-        g.setColor( Color.WHITE );
-        g.fillRect( 0, 0, width, height );
-        g.setColor( Color.BLACK );
-        // (posição x,posição y, tamanho x,tamanho y)
-        g.drawOval(50, 120, 75, 100);
-        g.drawOval(120, 50, 100, 75);
-        g.drawRect(200, 200, 200, 200);
-        g.create(250, 100, 300, 300);
-        return new ImageIcon( buffer );
-    }
+        
+        Draw draw = new Draw();
 
-    private static ImageIcon criarImagem2() {
         // int width=1024, height=768;
         int width=300,height=300;
         BufferedImage buffer = new BufferedImage( width, height, BufferedImage.TYPE_INT_RGB );
@@ -79,10 +62,19 @@ public class main{
         g.fillRect( 0, 0, width, height );
         g.setColor( Color.BLACK );
         // (posição x,posição y, tamanho x,tamanho y)
-        g.drawOval(50, 50, 50, 50);
-        g.drawRect(100, 100, 100, 100);
-        g.create(200, 100, 250, 300);
+
+        int [] draws = draw.get(aux++);
+        g.drawOval(draws[0], draws[1], draws[2], draws[3]);
+
+        draws = draw.get(aux++);
+        g.drawOval(draws[0], draws[1], draws[2], draws[3]);
+
+        draws = draw.get(aux++);
+        g.drawRect(draws[0], draws[1], draws[2], draws[3]);
+
+        // g.create(200, 100, 300, 300);
         return new ImageIcon( buffer );
     }
 
 }
+
