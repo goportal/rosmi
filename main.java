ec1c1a94ac7fab3843e19115102acbd26ddea0fa
portal <gabriel.o.portal@gmail.com>
2018-06-21 08:47:26 -0300

    ajuste

--- main.java
+++ main.java
@@ -13,16 +13,6 @@ public class main{
         frm.show();
     }
 
-    private static ImageIcon criarImagens(){
-        int width=900,height=600;
-        BufferedImage buffer = new BufferedImage( width, height, BufferedImage.TYPE_INT_RGB );
-        Graphics g = buffer.createGraphics();
-        g.setColor( Color.WHITE );
-        g.fillRect( 0, 0, width, height );
-        g.setColor(Color.BLACK);
-        g.drawImage(criarImagem(), 100, 1000, ImageObserver observer);
-    }
-
     private static ImageIcon criarImagem() {
         // int width=1024, height=768;
         int width=300,height=300;
@@ -31,11 +21,10 @@ public class main{
         g.setColor( Color.WHITE );
         g.fillRect( 0, 0, width, height );
         g.setColor( Color.BLACK );
-        // g.drawLine( 0, 0, width, height );
+        // (posição x,posição y, tamanho x,tamanho y)
         g.drawOval(50, 50, 50, 50);
         g.drawRect(100, 100, 100, 100);
-        g.create(150, 100, 250, 300);
-        // drawRect(int x, int y, int width, int height)
+        g.create(200, 100, 250, 300);
         return new ImageIcon( buffer );
     }
 
