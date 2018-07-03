3272ffc924b94573ddfc66a1d6935efda10fb23d
portal <gabriel.o.portal@gmail.com>
2018-07-03 16:19:36 -0300

    fix

--- parallel/MpSoc.java
+++ parallel/MpSoc.java
@@ -5,9 +5,9 @@ public class MpSoc{
     MpSoc(){
         cProcessor = new CentralProcessor(processors);
         for(int I=0;I<processors.length;I++){
-            processors[I] = new Processor(I,cProcessor);
-            cProcessor.start();
+            processors[I] = new Processor(I,cProcessor);       
         }
+        cProcessor.start();
         for(int I=0;I<processors.length;I++){
             processors[I].start();
         }
